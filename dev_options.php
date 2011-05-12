08df8c9b217829cb69c7da65b1206f4cf08a4005
cedric@yterium.com <>
2011-05-12 15:54:28 +0000

    en developpement, afficher un indicateur de memoire consommee en fin de hit, dans l'espace prive

--- dev_options.php
+++ dev_options.php
@@ -2,3 +2,11 @@
 
 define('_LOG_FILTRE_GRAVITE',8);
 $GLOBALS['test_i18n'] = true; // signaler les trads manquantes
+
+function affiche_usage_memoire(){
+	if (test_espace_prive())
+		echo "<div style='position:fixed;top:0;right:0;color:#fff;background:#666;padding:5px;z-index:1000;'>"
+		 . taille_en_octets(memory_get_usage())
+		 . '</div>';
+}
+register_shutdown_function('affiche_usage_memoire');
