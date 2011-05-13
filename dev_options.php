44f14ae40bb17f33244b50e7602558b1b7ffc11e
denisb@laposte.net <>
2011-05-13 14:12:21 +0000

    le grand retour de l'apostrophe ; ponctuation (et autre tricheries...)

--- dev_options.php
+++ dev_options.php
@@ -5,7 +5,7 @@ $GLOBALS['test_i18n'] = true; // signaler les trads manquantes
 
 function affiche_usage_memoire(){
 	if (test_espace_prive())
-		echo "<div title='Mémoire consommée, géré par l extension dev,memoire doit se situer en dessous de 12 idealement' style='position:fixed;top:0;right:0;color:#fff;background:#666;padding:5px;z-index:1000;'>"
+		echo "<div title=\"Mémoire consommée, géré par l'extension dev. Idéalement mémoire doit se situer en-dessous de 12 Mo.\" style='position:fixed;top:0;right:0;color:#fff;background:#666;padding:5px;z-index:1000;'>"
 		 . taille_en_octets(memory_get_usage())
 		 . '</div>';
 }
