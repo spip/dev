59115e5ed4cd40806ead1cb822ab4fc1e03bb98f
denisb@laposte.net <>
2011-12-31 12:33:48 +0000

    une fonction pour joliment afficher les variables de #ENV, #GET,  #SESSION... (copiée sur la fonction du débugueur : debusquer_contexte)

--- dev_options.php
+++ dev_options.php
@@ -18,3 +18,18 @@ function affiche_usage_memoire(){
 		echo var_export($GLOBALS['_debug'],true);
 }
 register_shutdown_function('affiche_usage_memoire');
+
+// Une fonction pour joliment afficher les variables de #ENV, #GET, #SESSION...
+// à utiliser avec [(#ENV|bel_env)], [(#GET|bel_env)], [(#SESSION|bel_env)]
+function bel_env($env) {
+  $env = str_replace(array('&quot;', '&#039;'), array('"', '\''), $env);
+  if (is_array($env_tab = @unserialize($env))) $env = $env_tab;
+  if (!$env) return '';
+  $res = "\n";
+  foreach ($env as $nom => $valeur) {
+    if (is_array($valeur)) $valeur = bel_env($valeur);
+    else $valeur = entites_html($valeur);
+    $res .= "|". entites_html($nom). "&nbsp;:&nbsp;|{" .$valeur. "}|\n";
+  }
+  return "\n<fieldset>\n" .propre($res). "</fieldset>\n";
+}
