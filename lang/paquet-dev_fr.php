c66bf319196fb60d6109322a46e13ea1d0d908a2
salvatore@rezo.net <>
2012-01-05 08:09:22 +0000

    langues (paquet-dev)

--- lang/paquet-dev_fr.php
+++ lang/paquet-dev_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-dev
-// Langue: fr
-// Date: 30-07-2011 15:01:36
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/dev/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'dev_description' => 'Outils de developpement',
-	'dev_slogan' => 'Outils de developpement',
+	'dev_slogan' => 'Outils de developpement'
 );
+
 ?>
