a245f1689598a3ae9e746bf143579d301c5bd85b
denisb@laposte.net <>
2012-01-11 14:54:44 +0000

    dev : corrections de pétouilles (typo et ortho).

--- lang/charter_fr.php
+++ lang/charter_fr.php
@@ -9,7 +9,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_checkbox' => 'Un choix multiple',
 	'label_checkbox_1' => 'Un premier choix',
 	'label_checkbox_2' => 'Un deuxième choix',
-	'label_checkbox_long_label' => 'Un choix multiple avec un tres tres long label qui utilise une class <tt>.long_label</tt>',
+	'label_checkbox_long_label' => 'Un choix multiple avec un très très long label qui utilise une class <tt>.long_label</tt>',
 	'label_checkbox_long_label_1' => 'Un premier choix',
 	'label_checkbox_long_label_2' => 'Un deuxième choix',
 	'label_radio' => 'Un choix unique',
@@ -19,12 +19,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_select_non' => 'Non merci, pas du tout',
 	'label_select_oui' => 'C\'est cela, OUI',
 	'label_text' => 'Une saisie en text',
-	'label_text_long_label' => 'Une saisie avec un tres tres long label qui utilise une class <tt>.long_label</tt>',
+	'label_text_long_label' => 'Une saisie avec un très très long label qui utilise une class <tt>.long_label</tt>',
 	'label_text_obli' => 'Une saisie obligatoire',
 	'label_textarea' => 'Une saisie en bloc',
 	'label_textarea_pleine_largeur' => 'Une saisie en bloc en <tt>.pleine_largeur</tt>',
 	'label_textarea_pleine_largeur_obli' => 'Une saisie obligatoire en bloc en <tt>.pleine_largeur</tt>',
-	'legend' => 'Legende d\'un fieldset, pour un groupe de saisie',
+	'legend' => 'Légende d\'un fieldset, pour un groupe de saisie',
 
 	// T
 	'titre_boites' => 'Boîtes',
