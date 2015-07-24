d7b6c5d3c49148a8ceda2fac33005d651b07b59e
nicolas.dorigny@gmail.com <>
2015-07-24 15:25:45 +0000

    Ajout du nombre de requêtes totales dans le bandeau d'infos. Le bandeau s'affiche sur toutes les pages (public et privé) mais uniquement pour les webmestres. Je l'ai repositionné à gauche plutôt qu'à droite, sinon il passe sous le minibando. Si on déclare $GLOBALS['page_start_time'] = microtime(true); le plus tôt possible (dans mes_options par exemple) on affiche aussi le temps total de chargement de la page. J'ai laissé le chdir(_ROOT_CWD); mais je n'ai pas compris quelle précaution il ajoutait.

--- dev_options.php
+++ dev_options.php
@@ -11,19 +11,31 @@ if(!defined('_DEBUG_MINIPRES'))
 	define('_DEBUG_MINIPRES',true);
 
 function affiche_usage_memoire(){
-	if (!defined('_AJAX') OR !_AJAX){
+	if (
+		(!defined('_AJAX') OR !_AJAX)
+		AND $GLOBALS['auteur_session']['webmestre'] == 'oui'
+		AND !_request('action')
+		AND !preg_match('#(\.css|\.js)#', _request('page'))
+		AND !preg_match('#(\.css|\.js)#', _request('file'))
+	) {
 		chdir(_ROOT_CWD); // precaution
-		// dans l'espace prive uniquement, et si la fonction taille_en_octets est deja chargee
-		if (test_espace_prive()
-		    AND function_exists('taille_en_octets')
-				AND !_request('action'))
-			echo "<div style='position:fixed;top:0;right:0;color:#fff;background:#666;padding:5px;z-index:1010;'>"
-			 . taille_en_octets(memory_get_usage())
-			 . '</div>';
-		if (isset($GLOBALS['_debug']))
-			echo var_export($GLOBALS['_debug'],true);
+		echo "<div style='position:fixed;top:0;left:0;color:#fff;background:#666;padding:5px;z-index:1010;'>";
+		echo round(memory_get_usage() / 1024 / 1024, 1) . ' Mo';
+		foreach ($GLOBALS['connexions'] as $serveur => $connexion) {
+			if (isset($connexion['total_requetes'])) {
+				echo " | " . ($serveur ? $serveur . " : " : "") . $connexion['total_requetes'] . " requetes";
+			}
+		}
+		// définir $GLOBALS['page_start_time'] = microtime(true); le plus tôt possible (dans mes_options)
+		// pour afficher le temps de calcul de la page
+		if (isset($GLOBALS['page_start_time'])) {
+			echo " | " . number_format((microtime(true) - $GLOBALS['page_start_time']), 2) . " s";
+		}
+		echo "</div>";
+		if (isset($GLOBALS['_debug'])) {
+			echo var_export($GLOBALS['_debug'], true);
+		}
 	}
 }
 register_shutdown_function('affiche_usage_memoire');
 
-?>
