84c64829a6ec1b3ebae37fd5e2e78c61918c1f9a
rporto@calepino.com <>
2016-09-27 03:32:58 +0000

    [Salvatore] [source:_core_/plugins/dev/lang/ charter] Export depuis http://trad.spip.net de la langue pt_br

---
+++ lang/charter_pt_br.php
@@ -0,0 +1,42 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/charter?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'label_checkbox' => 'Uma escolha múltipla',
+	'label_checkbox_1' => 'Uma primeira escolha',
+	'label_checkbox_2' => 'Uma segunda escolha',
+	'label_checkbox_long_label' => 'Uma escolha múltipla com um label muito longo, usando uma classe <tt>.long_label</tt>',
+	'label_checkbox_long_label_1' => 'Uma primeira escolha',
+	'label_checkbox_long_label_2' => 'Uma segunda escolha',
+	'label_checkbox_ouiounon' => 'Uma escolha a marcar',
+	'label_checkbox_ouiounon_oui' => 'Sim',
+	'label_radio' => 'Uma escolha única',
+	'label_radio_non' => 'Não',
+	'label_radio_oui' => 'Sim',
+	'label_select' => 'Uma entrada de dados na forma de seletor',
+	'label_select_non' => 'Não, obrigado, de modo algum',
+	'label_select_oui' => 'SIM, isso mesmo',
+	'label_text' => 'Uma entrada de dados em texto',
+	'label_text_long_label' => 'Uma entrada de dados com um label muito longo, que usa uma classe <tt>.long_label</tt>',
+	'label_text_obli' => 'Uma entrada de dados obrigatória',
+	'label_textarea' => 'Uma entrada de dados em bloco',
+	'label_textarea_pleine_largeur' => 'Uma entrada de dados em bloco com largura total <tt>.pleine_largeur</tt>',
+	'label_textarea_pleine_largeur_obli' => 'Uma entrada de dados em bloco obrigatória com largura total<tt>.pleine_largeur</tt>',
+	'legend' => 'Legenda de um fieldset, para um grupo de entradas de dados',
+
+	// T
+	'titre_boites' => 'Boxes',
+	'titre_charte' => 'Charte', # RELIRE
+	'titre_formulaires' => 'Formulários',
+	'titre_icones' => 'Icones',
+	'titre_listes' => 'Listas',
+	'titre_typo' => 'Tipografia'
+);
