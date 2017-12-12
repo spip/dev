c537004a50442aeb52f2b453b0601ad0bfa1d5bf
marcimat@rezo.net <>
2017-12-12 12:25:23 +0000

    Ticket #4060 : nettoyage de code < PHP 5.4

--- dev_options.php
+++ dev_options.php
@@ -28,13 +28,7 @@ function affiche_usage_memoire() {
 				echo " | " . ($serveur ? $serveur . " : " : "") . $connexion['total_requetes'] . " requetes";
 			}
 		}
-		if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
-			echo " | " . number_format((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 2) . " s";
-		} else if (isset($GLOBALS['page_start_time'])) {
-			// définir $GLOBALS['page_start_time'] = microtime(true); le plus tôt possible (dans mes_options)
-			// pour afficher le temps de calcul de la page
-			echo " | " . number_format((microtime(true) - $GLOBALS['page_start_time']), 2) . " s";
-		}
+		echo " | " . number_format((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 2) . " s";
 		echo "</div>";
 		if (isset($GLOBALS['_debug'])) {
 			echo var_export($GLOBALS['_debug'], true);
